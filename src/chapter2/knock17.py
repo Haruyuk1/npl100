f2fb1f340110fdfd526a3c0ae88769aed0bfb02e
Haruyuk1 <sa990130@gmail.com>
2022-07-07 15:10:24 +0900

    knock17-19

---
+++ src/chapter2/knock17.py
@@ -0,0 +1,21 @@
+from io import TextIOWrapper
+import os
+
+
+def main(f: TextIOWrapper):
+    source_txt = "data/popular-names.txt"
+    with open(source_txt, mode="r") as f_source:
+        lines = f_source.readlines()
+    splitted_lines = [line.split("\t") for line in lines]
+    col1 = [line[0] for line in splitted_lines]
+    print(set(col1))
+    return
+
+
+if __name__ == "__main__":
+    # hogehoge.pyだった場合、result/hogehoge.txtへのTextIOWrapperをmainに渡す
+    target_file_name: str = os.path.basename(__file__).replace(".py", "")
+    target_folder_name: str = "result"
+    target_file_path: str = target_folder_name + "/" + target_file_name + ".txt"   
+    with open(target_file_path, mode="w", encoding="utf-8") as f:
+        main(f)
---
+++ src/chapter2/knock18.py
@@ -0,0 +1,21 @@
+import os
+from io import TextIOWrapper
+
+import pandas as pd
+
+
+def main(f: TextIOWrapper):
+    source_txt = "data/popular-names.txt"
+    df = pd.read_table(source_txt, header=None)
+    print(df.sort_values(2, ascending=False))
+
+    return
+
+
+if __name__ == "__main__":
+    # hogehoge.pyだった場合、result/hogehoge.txtへのTextIOWrapperをmainに渡す
+    target_file_name: str = os.path.basename(__file__).replace(".py", "")
+    target_folder_name: str = "result"
+    target_file_path: str = target_folder_name + "/" + target_file_name + ".txt"   
+    with open(target_file_path, mode="w", encoding="utf-8") as f:
+        main(f)
